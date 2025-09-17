af49db3af716d38f6a2846dea8cada4d11810a51
Zamzam100 <kassimzamirah@gmail.com>
2025-09-17 01:20:01 -0700

    first commit

---
+++ day_1.py
@@ -0,0 +1,27 @@
+print(3+4) #addition
+print(3-4) #subtraction
+print(3*4) #multiplication
+print(3/4) #division
+print(3//4) #floor division
+print(3%4) #modulus
+print(3**4) #exponentiation
+
+#write the strings
+print("My name is")
+print("Zamirah") #string
+print("My name is" + " Zamirah") #string concatenation
+print('Wekhoba') #string
+print("Zamirah" + " Wekhoba") #string concatenation
+print("Kenya")
+print("Kenya" + " is my country")
+print("I'm enjoying 30 days of python") #string with single quote inside double quotes
+
+#check data types
+print(type(10)) #integer
+print(type(9.8)) #float
+print(type(3.14)) #float
+print(type(4-4j)) #complex number
+print(type(['Asabeneh', 'Python', 'Finland'])) #list
+print(type('Zamirah')) #string
+print(type('Wekhoba')) #string
+print(type('Kenya')) #string
---
+++ helloworld.py
@@ -0,0 +1,23 @@
+print(3+2) #addition
+print(3-2) #subtraction
+print(3*2) #multiplication
+print(3/2) #division
+print(3//2) #floor division
+print(3%2) #modulus
+print(3**2) #exponentiation
+print((3+2)*2) #using parentheses to change order of operations
+print(3+2*2) #order of operations without parentheses
+print(3**2*2) #order of operations with exponentiation
+
+#Checking data types
+print(type(3)) #integer
+print(type(3.0)) #float
+print(type(3/2)) #float
+print(type(3//2)) #integer
+print(type(3%2)) #integer
+print(type("Hello World")) #string
+print(type(True)) #boolean
+print(type((3, 2))) #tuple
+print(type([3, 2])) #list
+print(type({3: "three", 2: "two"})) #dictionary
+print(type({3, 2})) #set
